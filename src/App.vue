24b7fe1938d5193dfc6d12e9cf6c9cdfbbd5652b
JimmyShukurow <jimishukurow@gmail.com>
2022-04-09 16:39:31 +0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class="profession">
+      Software developer 
+    </span>
+    
+    <div style="display:flex; justify-content:center">
+        <div class="animeClass">
+          <img class="photo" alt="Vue logo" src="./assets/photo.jpeg">
+          <!-- menu items start here -->
+          <img src="./assets/avatar.png" alt="menu1"  id="menu1"  class="menu-image " >
+          <img src="./assets/avatar.png" alt="menu2"  id="menu2" class="menu-image  " >
+          <img src="./assets/avatar.png" alt="menu3"  id="menu3"  class="menu-image " >
+          <img src="./assets/avatar.png" alt="menu4"  id="menu4" class="menu-image  " >
+          <img src="./assets/avatar.png" alt="menu5"  id="menu5" class="menu-image  " >
+          <img src="./assets/github.png" alt="menu6"  id="menu6" class="menu-image  " >
+
+          <!-- end of menu items -->
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import forexport from '@/js/animate';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  mounted() {
+      forexport.menuOpen();
   }
 }
 </script>
@@ -25,4 +42,35 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.photo {
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+  border: 1px solid gray;
+}
+.menu-image{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    position: absolute;
+    top: 230px;
+    left: 230px;
+}
+.animeClass {
+  margin-top: 50px;
+  width: 250px;
+  height: 250px;
+  position: relative;
+}
+
+.profession {
+  font-size: 2rem;
+  font-weight: 100;
+  font-style: normal;
+}
+
+</style>
+
+<style lang="scss">
+ @import "./scss/background.scss";
 </style>
---
+++ src/js/animate.js
@@ -0,0 +1,96 @@
+var id1 = null;
+var id2 = null;
+var id3 = null;
+var id4 = null;
+var id5 = null;
+var id6 = null;
+var radius = Math.sqrt(2) * 125;  
+var oneDegree = Math.PI / 180;
+
+var forexport = {
+     menuOpen() {
+        let xStart = 105;
+        let yStart = 105;
+        let startAngle = Math.PI / 6;
+        let intervalAngle = Math.PI / 3;
+        var elem1 = document.getElementById('menu1'); 
+        var elem2 = document.getElementById('menu2'); 
+        var elem3 = document.getElementById('menu3'); 
+        var elem4 = document.getElementById('menu4'); 
+        var elem5 = document.getElementById('menu5'); 
+        var elem6 = document.getElementById('menu6'); 
+        var angle = 0;
+        clearInterval(id1);
+        clearInterval(id2);
+        clearInterval(id3);
+        clearInterval(id4);
+        clearInterval(id5);
+        clearInterval(id6);
+        id1 = setInterval(frame1, 1);
+        id2 = setInterval(frame2, 1);
+        id3 = setInterval(frame3, 1);
+        id4 = setInterval(frame4, 1);
+        id5 = setInterval(frame5, 1);
+        id6 = setInterval(frame6, 1);
+
+        
+        function frame1() {
+          if (angle >= startAngle) {
+            clearInterval(id1);
+          } else {
+            angle = angle + oneDegree;
+            elem1.style.top = (yStart + radius*Math.cos(angle)) + "px"; 
+            elem1.style.left = (xStart + radius*Math.sin(angle))+"px"; 
+          }
+        }
+        function frame2() {
+          if (angle >= (startAngle +intervalAngle)) {
+            clearInterval(id2);
+          } else {
+            angle = angle + oneDegree;
+            elem2.style.top = (yStart + radius*Math.cos(angle)) + "px"; 
+            elem2.style.left = (xStart + radius*Math.sin(angle))+"px"; 
+          }
+        }
+        function frame3() {
+          if (angle >= (startAngle + 2*intervalAngle)) {
+            clearInterval(id3);
+          } else {
+            angle = angle + oneDegree;
+            elem3.style.top = (yStart + radius*Math.cos(angle)) + "px"; 
+            elem3.style.left = (xStart + radius*Math.sin(angle))+"px"; 
+          }
+        }
+        function frame4() {
+          if (angle >=  startAngle + 3*intervalAngle) {
+            clearInterval(id4);
+          } else {
+            angle = angle + oneDegree;
+            elem4.style.top = (yStart + radius*Math.cos(angle)) + "px"; 
+            elem4.style.left = (xStart + radius*Math.sin(angle))+"px"; 
+          }
+        }
+        function frame5() {
+          if (angle >= startAngle + 4*intervalAngle) {
+            clearInterval(id5);
+          } else {
+            angle = angle + oneDegree;
+            elem5.style.top = (yStart + radius*Math.cos(angle)) + "px"; 
+            elem5.style.left = (xStart + radius*Math.sin(angle))+"px"; 
+          }
+        }
+        function frame6() {
+            if (angle  >=  startAngle + 5*intervalAngle) {
+              clearInterval(id6);
+            } else {
+              angle = angle + oneDegree;
+              elem6.style.top = (yStart + radius*Math.cos(angle)) + "px"; 
+              elem6.style.left = (xStart + radius*Math.sin(angle))+"px"; 
+            }
+          }
+      }
+}
+
+
+
+  export default forexport
