8cb10d58a424e0745b23ca4369ab9331b6721895
JimmyShukurow <jimishukurow@gmail.com>
2022-04-10 05:31:05 +0300

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ]
-})
+});
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/newResume/" : "/",
+};
