26de7f9407d75a0056f7e0b92698039e79d82ce6
JimmyShukurow <jimishukurow@gmail.com>
2022-04-22 11:26:43 +0300

    ok

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -2,61 +2,71 @@
   <div class="home">
     <v-container class="main">
       <header>
-        <a
-          href="https://jimmyshukurow.github.io/Resume-Static/"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a href="https://jimmyshukurow.github.io/Resume-Static/" target="_blank"
           ><v-btn> old resume </v-btn></a
         >
         <router-link to="/"> <v-btn> get back </v-btn> </router-link>
       </header>
-      <v-row cols="8">
-        <v-col cols="12" sm="4">
+
+      <h1>Vue Fun Projects</h1>
+      <br />
+
+      <v-row cols="8" class="rows">
+        <v-col
+          cols="12"
+          sm="4"
+          v-for="(vueProject, index) in vueProjects"
+          :key="index"
+        >
           <v-card color="#385F73" dark>
-            <v-card-title class="text-h5"> Vuex Example Project </v-card-title>
+            <v-card-title class="text-h5"> {{ vueProject.title }}</v-card-title>
 
             <v-card-subtitle>
-              Project done using vue to demonstrate Vuex and its benefits. Very
-              little Project.
+              {{ vueProject.description }}
             </v-card-subtitle>
 
             <v-card-actions>
-              <v-btn text>
-                <a
-                  href="https://jimmyshukurow.github.io/Vuex/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Details</a
-                >
-              </v-btn>
+              <a :href="vueProject.details" target="_blank">
+                <v-btn text> Details </v-btn>
+              </a>
+              <v-spacer></v-spacer>
+              <a :href="vueProject.githubLink" target="_blank">
+                <v-btn text> Github </v-btn>
+              </a>
             </v-card-actions>
           </v-card>
         </v-col>
-        <v-col cols="12" sm="4">
-          <v-card color="#385F73" dark>
-            <v-card-title class="text-h5"> Unlimited music now </v-card-title>
+      </v-row>
 
-            <v-card-subtitle
-              >Listen to your favorite artists and albums whenever and wherever,
-              online and offline.</v-card-subtitle
+      <h1>Laravel Projects</h1>
+      <br />
+      <v-row cols="8">
+        <v-col
+          cols="12"
+          sm="4"
+          v-for="(laravelProject, index) in laravelProjects"
+          :key="index"
+        >
+          <v-card color="#385F73" dark>
+            <v-card-title class="text-h5">
+              {{ laravelProject.title }}</v-card-title
             >
 
-            <v-card-actions>
-              <v-btn text> Listen Now </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-        <v-col cols="12" sm="4">
-          <v-card color="#385F73" dark>
-            <v-card-title class="text-h5"> Unlimited music now </v-card-title>
-
-            <v-card-subtitle
-              >Listen to your favorite artists and albums whenever and wherever,
-              online and offline.</v-card-subtitle
-            >
+            <v-card-subtitle>
+              {{ laravelProject.description }}
+            </v-card-subtitle>
 
             <v-card-actions>
-              <v-btn text> Listen Now </v-btn>
+              <a :href="laravelProject.details" target="_blank">
+                <v-btn text> Details </v-btn>
+              </a>
+              <v-spacer></v-spacer>
+              <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                  <v-btn text  v-on="on"> Github </v-btn>
+                </template>
+                <h1>Top tooltip</h1>
+              </v-tooltip>
             </v-card-actions>
           </v-card>
         </v-col>
@@ -71,6 +81,61 @@
 export default {
   name: "HomeView",
   components: {},
+  data: () => ({
+    vueProjects: [
+      {
+        title: "Vuex Example Project",
+        description:
+          "Project done using vue to demonstrate Vuex and its benefits. Very little Project.",
+        details: "https://jimmyshukurow.github.io/Vuex/",
+        githubLink: "https://github.com/JimmyShukurow/Vuex",
+      },
+      {
+        title: "Vue Test Project",
+        description:
+          "Project done using vue to demonstrate axios and its usage with javascript promises. Check it out ",
+        details: "https://jimmyshukurow.github.io/VueProject/",
+        githubLink: "https://github.com/JimmyShukurow/VueProject",
+      },
+      {
+        title: "Vue Admin Panel (Not Finished)",
+        description:
+          "This is an admin panel which is planned to use in all my Vue projects. Actually its not going well ...",
+        details: "https://vue-admin-panel.vercel.app/#/",
+        githubLink: "https://github.com/JimmyShukurow/VueAdminPanel",
+      },
+    ],
+    laravelProjects: [
+      {
+        title: "Perpa Expo Laravel",
+        description:
+          "This Project is done using Laravel and Jquery. But I cant share codes, because of privacy policy",
+        details: "https://perpaexpo.com/",
+        githubLink: "#",
+      },
+      {
+        title: "Travelshop Booking Laravel",
+        description:
+          "This Project is done using Laravel and Vue. But I cant share codes, because of privacy policy",
+        details: "https://travelshopbooking.com/",
+        githubLink: "#",
+      },
+      {
+        title: "Ageenta Laravel",
+        description:
+          "This Project is done using Laravel and React. But I cant share codes, because of privacy policy",
+        details: "https://www.ageenta.com",
+        githubLink: "#",
+      },
+      {
+        title: "Second Hand Car Laravel",
+        description:
+          "This Project is done using Laravel and React. But I cant share codes, because of privacy policy",
+        details: "https://incirogluikincielbirinci.com/",
+        githubLink: "#",
+      },
+    ],
+  }),
 };
 </script>
 
@@ -86,6 +151,9 @@ a {
 }
 .mycard {
   background-color: brown;
+}
+.rows {
+  margin-bottom: 75px;
 }
 </style>
 
